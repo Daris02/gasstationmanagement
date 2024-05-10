5eeae21a818e4453fa7aa522442c6c68b06b959c
Daris02 <hei.raymond.2@gmail.com>
2024-05-10 11:15:17 +0300

    fix: finish supply and sale

---
+++ src/main/java/com/hei/app/gasstationmanagement/Exception/QuantityExcessExcpetion.java
@@ -0,0 +1,8 @@
+package com.hei.app.gasstationmanagement.Exception;
+
+public class QuantityExcessExcpetion extends RuntimeException{
+
+    public QuantityExcessExcpetion(String message) {
+        super((message));
+    }
+}
---
+++ src/main/java/com/hei/app/gasstationmanagement/config/DefaultValue.java
@@ -0,0 +1,5 @@
+package com.hei.app.gasstationmanagement.config;
+
+public class DefaultValue {
+    public static final Double QUANTITY_MAX = 200.0;
+}
